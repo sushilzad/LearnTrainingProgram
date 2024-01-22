8899f2ee1cc9a76aaa6f3534351f6331f10bcab8
Sushil Sunil Zad <sushilsunil_zad@epam.com>
2024-01-22 16:43:54 +0530

    Topic3_Task2_CSharpKeywords_Examples

--- ProgramTopics/Program.cs
+++ ProgramTopics/Program.cs
@@ -8,13 +8,24 @@ namespace ProgramTopics
 {
     internal class Program
     {
+        static void DisplayMessageOnConsole(string message)
+        {
+            Console.WriteLine("\n\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> " + message + " <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+        }
+
         static void Main(string[] args)
         {
+            DisplayMessageOnConsole("TOPIC 2");
             Topic2_ObjectOrientedConcepts_Advanced topic2 = new Topic2_ObjectOrientedConcepts_Advanced();
             topic2.Task1_Demo_Abstraction();
 
+            DisplayMessageOnConsole("TOPIC 3 - TASK 1");
             Topic3.Task1_StringOperations task1_StringOperations = new Topic3.Task1_StringOperations();
             task1_StringOperations.PerformStringOperations();
+
+            DisplayMessageOnConsole("TOPIC 3 - TASK 2");
+            Topic3.Task2_CSharpKeywords task2_CSharpKeywords = new Topic3.Task2_CSharpKeywords();
+            task2_CSharpKeywords.PerformOperationUsingCSharpKeywords();
 
             Console.ReadKey();
         }
